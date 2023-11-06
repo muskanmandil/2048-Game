d10eac2caf461e8b8a66be0b7117834544d9261e
Muskan Mandil <muskan05mandil@gmail.com>
2023-11-06 18:39:22 +0530

    fixed an error

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const updateScore = (value) => {
 
 const moveUp = () => {
     let trueArr = Array.from(document.getElementsByClassName("true"));
-    
+
     trueArr.forEach(element => {
 
         let pos = Number.parseInt(element.id);
@@ -242,22 +242,26 @@ document.addEventListener("keydown", (event) => {
     switch (event.key) {
         case 'ArrowUp':
             console.log("up");
-            moveUp()
+            moveUp();
+            generateNewPosition();
             break;
 
         case 'ArrowDown':
             console.log("down");
-            moveDown()
+            moveDown();
+            generateNewPosition();
             break;
 
         case 'ArrowLeft':
             console.log("left");
             moveLeft();
+            generateNewPosition();
             break;
 
         case 'ArrowRight':
             console.log("right");
             moveRight();
+            generateNewPosition();
             break;
     }
 });
